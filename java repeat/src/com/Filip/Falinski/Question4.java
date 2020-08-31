7ad68ebf54db6f3d76713468a743bc489800b953
FilipFalinski <55143687+FilipFalinski@users.noreply.github.com>
2020-08-31 12:15:18 +0100

    Add files via upload

--- java repeat/src/com/Filip/Falinski/Question4.java
+++ java repeat/src/com/Filip/Falinski/Question4.java
@@ -1,4 +1,25 @@
 package com.Filip.Falinski;
 
+import javax.swing.*;
+
 public class Question4 {
+    public static void main (String[] args)
+    {
+        // String s = "fred";  // use this if you want to test the exception below
+        String s = "age";
+
+        float memSize = Integer.parseInt(JOptionPane.showInputDialog("Enter Age"));
+        try
+        {
+            // the String to int conversion happens here
+            int i = Integer.parseInt(s.trim());
+
+            // print out the value after the conversion
+            System.out.println("int i = " + i);
+        }
+        catch (NumberFormatException nfe)
+        {
+            System.out.println("NumberFormatException: " + nfe.getMessage());
+        }
+    }
 }
--- java repeat/src/com/Filip/Falinski/Qusetion2.java
+++ java repeat/src/com/Filip/Falinski/Qusetion2.java
@@ -1,4 +1,7 @@
 package com.Filip.Falinski;
 
-public class Qusetion2 {
+public class Qusetion2 extends Student {
+    public Qusetion2(String studentId, String firstName, String lastName, int age, int grade1, int grade2, int grade3) {
+        super(studentId, firstName, lastName, age, grade1, grade2, grade3);
+    }
 }
