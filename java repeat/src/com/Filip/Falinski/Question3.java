e58ad0fbf7fa150d795b1e24d4ea7214a0925eb5
FilipFalinski <55143687+FilipFalinski@users.noreply.github.com>
2020-08-31 11:47:31 +0100

    Add files via upload

--- java repeat/src/com/Filip/Falinski/Question3.java
+++ java repeat/src/com/Filip/Falinski/Question3.java
@@ -1,12 +1,14 @@
 package com.Filip.Falinski;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JFrame;
 import javax.swing.*;
 import javax.swing.border.Border;
 
 
-public class Question3 extends JFrame  {
+public class Question3 extends JFrame  implements ActionListener {
 
     public Question3() {
 
@@ -63,4 +65,39 @@ public class Question3 extends JFrame  {
             ex.setVisible(true);
         });
     }
+
+    @Override
+    public void actionPerformed(ActionEvent actionEvent) {
+
+    }
 }
+
+
+//inheritance is when you can make a classes that are built on existing classes meaning that when a class inherits something form another class
+//it can reuse that method. a subclass can inherit all the public and protected mehtods form the parent class but it cannot iherit private
+//example would be.
+// class Animal {
+//        float weight;
+//        ...
+//        void eat() {
+//            ...
+//        }
+//        ...
+//    }
+//
+//    class Mammal extends Animal {
+//        // inherits weight
+//        int heartRate;
+//        ...
+//
+//        // inherits eat()
+//        void breathe() {
+
+// a subclass can be further subclassed
+//class Cat extends Mammal {
+    // inherits weight and heartRate
+   // boolean longHair;
+
+
+    // inherits eat() and breathe()
+    //void purr() {
